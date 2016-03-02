ce37463410c378f7782d47619666e9c2f7dd9b84
Magana-Zook, Steven Alan <maganazook1@llnl.gov>
2016-03-02 09:23:55 -0800

    Best function implemented and tested

--- R Programming Course/Programming Assignment 3/best.R
+++ R Programming Course/Programming Assignment 3/best.R
@@ -7,12 +7,17 @@ best <- function(state, outcome) {
     stop("invalid state")
   }
   
-  if((outcome %in% c("heart attack", "heart failure", "pneumonia")) == FALSE){
+  outcomes <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23) 
+  if((outcome %in% names(outcomes)) == FALSE){
     stop("invalid outcome")
   }
   
   ## Return hospital name in that state with lowest 30-day death
-  subdata = subset(data, data$state == state)
-  View(subdata)
+  subdata = subset(data, data$State == state)[, c(2,7,outcomes[outcome])]
+  
+  subdata = na.omit(subdata)
+  subdata = subdata[order(subdata[,3]),] #sort by column code: http://stackoverflow.com/a/1296745/534347
+  
   ## rate
+  subdata[1,1]
 }
