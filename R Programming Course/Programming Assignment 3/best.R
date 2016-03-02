be26016fa82f93f53d9a2ddee172f9a6442b0bc2
Magana-Zook, Steven Alan <maganazook1@llnl.gov>
2016-03-02 09:30:13 -0800

    Added comments citing sources, and added a secondary sort on the hospital name.

--- R Programming Course/Programming Assignment 3/best.R
+++ R Programming Course/Programming Assignment 3/best.R
@@ -7,17 +7,24 @@ best <- function(state, outcome) {
     stop("invalid state")
   }
   
+  # Outcomes code from Al Warren's "[Tips] A few pointers for assignment 3"
   outcomes <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23) 
   if((outcome %in% names(outcomes)) == FALSE){
     stop("invalid outcome")
   }
   
-  ## Return hospital name in that state with lowest 30-day death
+  
+  # Shrink data to only have the three columns we care about
+  # Code from Al Warren's "[Tips] A few pointers for assignment 3"
   subdata = subset(data, data$State == state)[, c(2,7,outcomes[outcome])]
   
-  subdata = na.omit(subdata)
-  subdata = subdata[order(subdata[,3]),] #sort by column code: http://stackoverflow.com/a/1296745/534347
+  # Shrink data to only those without NA values
+  subdata = na.omit(subdata) 
   
-  ## rate
+  # Sort first by lowest mortality, then by hospital name
+  # Sort-by-column code: http://stackoverflow.com/a/1296745/534347
+  subdata = subdata[order(subdata[,3],subdata[,1]),] 
+
+  ## Return the hospital with the lowest mortality rate for the specified outcome
   subdata[1,1]
 }
